e7034dbcc14d45a062742580c4b857e3bd1947dd
Alex Gherman <alexgherman997@gmail.com>
2024-07-15 10:09:55 +0300

    project status

--- scrapy_jobboard/spiders/cw_jobs_spider.py
+++ scrapy_jobboard/spiders/cw_jobs_spider.py
@@ -1,6 +1,6 @@
 import scrapy
 
-#banned to many request at once, using proxy next time 
+
 class CWJobsSpider(scrapy.Spider):
     name = "cwjobs"
     start_urls = [
@@ -16,27 +16,28 @@ class CWJobsSpider(scrapy.Spider):
         pagination_links = response.css("[aria-label='Next']")
         yield from response.follow_all(pagination_links, self.parse)
 
-        # for jobs in response.css("[data-at='job-item']"):
-        #     yield {
-        #         "title": jobs.css('.res-1ewaude .res-nehv70::text').get(),
-        #         "company": jobs.css('.res-4xt73y .res-at01ei::text').get(),
-        #         "location": jobs.css('.res-1dil7qs .res-at01ei::text').get(),
-        #         "salaryFrom": jobs.css('.res-at01ei .res-4xt73y::text').get(),
-        #         "postedDate": jobs.css('time::text').get(),
-        #     }
+        for jobs in response.css("[data-at='job-item']"):
+             yield {
+                 "title": jobs.css('.res-1ewaude .res-nehv70::text').get(),
+                 "company": jobs.css('.res-4xt73y .res-at01ei::text').get(),
+                 "location": jobs.css('.res-1dil7qs .res-at01ei::text').get(),
+                 "salaryFrom": jobs.css('.res-at01ei .res-4xt73y::text').get(),
+                 "postedDate": jobs.css('time::text').get(),
+             }
 
     def parse_job_detail_page(self, response):
-      yield {
+        yield {
           "title": response.css('.job-ad-display-s80d2f::text').get(),
-          # "company": response.css('.res-1ewaude .res-nehv70::text').get(),
-          # "location": response.css('.res-1ewaude .res-nehv70::text').get(),
-          # "salaryFrom": response.css('.res-1ewaude .res-nehv70::text').get(),
-          # "postedDate": response.css('.res-1ewaude .res-nehv70::text').get(),
+           "company": response.css('.res-1ewaude .res-nehv70::text').get(),
+          "location": response.css('.res-1ewaude .res-nehv70::text').get(),
+           "salaryFrom": response.css('.res-1ewaude .res-nehv70::text').get(),
+           "postedDate": response.css('.res-1ewaude .res-nehv70::text').get(),
       }
 
+        job_parse_links = response.css("[data-testid='job-item-title'] a::attr(href)").get()
+        if job_parse_links is not None: 
+            job_parse_links = response.urljoin(job_parse_links)
+            yield scrapy.Request(job_parse_links, callback=self.parse)
 
-        # job_parse_links = response.css("[data-testid='job-item-title'] a::attr(href)").get()
-        
-        # if job_parse_links is not None: 
-        #     job_parse_links = response.urljoin(job_parse_links)
-        #     yield scrapy.Request(job_parse_links, callback=self.parse)
+
+# banned for 1 day to many request at once, using proxy next time 
