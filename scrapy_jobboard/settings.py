27a71e51ae99c1ae58d4dcb42d1bd9ddf29a892a
Alex Gherman <alexgherman997@gmail.com>
2024-07-15 09:52:14 +0300

    fixes and clean up

--- scrapy_jobboard/settings.py
+++ scrapy_jobboard/settings.py
@@ -50,12 +50,12 @@ ROBOTSTXT_OBEY = True
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-DOWNLOADER_MIDDLEWARES = {
-   "rotating_proxies.middlewares.RotatingProxyMiddleware": 300,
-   "rotating_proxies.middlewares.BanDetectionMiddleware": 301,
-}
+# DOWNLOADER_MIDDLEWARES = {
+#    "rotating_proxies.middlewares.RotatingProxyMiddleware": 300,
+#    "rotating_proxies.middlewares.BanDetectionMiddleware": 301,
+# }
 
-ROTATING_PROXY_LIST_PATH = 'proxies.txt'
+# ROTATING_PROXY_LIST_PATH = 'proxies.txt'
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
--- scrapy_jobboard/spiders/qoutes_spider.py
+++ scrapy_jobboard/spiders/qoutes_spider.py
@@ -1,62 +1,52 @@
 import scrapy
 
+
+
+
 class QuotesSpider(scrapy.Spider):
-    name = "quotes"
+    name = 'quotes'
     start_urls = [
-        "https://quotes.toscrape.com/page/1/",
-        "https://quotes.toscrape.com/page/2/",
+        'https://quotes.toscrape.com/page/1/',
+        'https://quotes.toscrape.com/page/2/',
     ]
 
+    user_agent_list = [
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.2 Safari/605.1.15',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0',
+    'Mozilla/5.0 (iPhone; CPU iPhone OS 14_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Mobile/15E148 Safari/604.1',
+    'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:89.0) Gecko/20100101 Firefox/89.0',
+    'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36 Vivaldi/4.0',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:89.0) Gecko/20100101 Firefox/89.0',
+    'Mozilla/5.0 (Linux; Android 11; SM-G991B) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Mobile Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.106 Safari/537.36 Edg/91.0.864.48',
+    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.0 Safari/605.1.15',
+    'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0',
+    'Mozilla/5.0 (iPad; CPU OS 13_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.2 Mobile/15E148 Safari/604.1',
+    'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:88.0) Gecko/20100101 Firefox/88.0',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1.2 Safari/605.1.15',
+    'Mozilla/5.0 (Linux; Android 10; SM-G973F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Mobile Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.85 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/601.7.7 (KHTML, like Gecko) Version/11.1.2 Safari/601.7.7',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36',
+    'Mozilla/5.0 (iPhone; CPU iPhone OS 14_4_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Mobile/15E148 Safari/604.1',
+    'Mozilla/4.0 (compatible; MSIE 9.0; Windows NT 6.1)',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36 Edg/87.0.664.75',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 Edge/18.18363',
+]
+
     def parse(self, response):
-        for quote in response.css("div.quote"):
+        for quote in response.css('div.quote'):
             yield {
-                "text": quote.css("span.text::text").get(),
-                "author": quote.css("small.author::text").get(),
-                "tags": quote.css("div.tags a.tag::text").getall(),
+                'text': quote.css('span.text::text').get(),
+                'author': quote.css('small.author::text').get(),
+                'tags': quote.css('div.tags a.tag::text').getall(),
             }
-        next_page = response.css("li.next a::attr(href)").get()
+        next_page = response.css('li.next a::attr(href)').get()
         if next_page is not None: 
             next_page = response.urljoin(next_page)
             yield scrapy.Request(next_page, callback=self.parse)
             
-          # my accoutn free  
-# 45.127.248.127:5128
-# 207.244.217.165:6712
-# 134.73.69.7:5997
-# 64.64.118.149:6732
-# 157.52.253.244:6204
-# 167.160.180.203:6754
-# 166.88.58.10:5735
-# 173.0.9.70:5653
-# 204.44.69.89:6342
-# 173.0.9.209:5792
-
-#internet free
-# 140.227.204.70:3128
-# 103.127.106.249:8090	
-# 130.61.120.213:8888
-# 134.209.29.120:3128
-# 132.148.245.112:19483
-# 102.220.142.193:8080
-# 102.215.197.202:9999
-# 101.255.167.110:3125
-# 1.179.151.165:31948
-# 103.111.22.65:31948
-
-
-#internet free
-# 1.0.170.50:8080
-# 137.59.5.171:6654
-# 103.105.196.30:80
-# 102.66.110.172:9999
-# 103.110.10.190:3128
-# 130.61.120.213:8888
-# 103.105.196.56:80
-# 130.255.162.199:44234		
-# 136.226.232.198:11197
-# 136.226.81.15:10230
-# 136.226.67.28:10160
-# 136.226.65.108:10160
-# 102.68.131.31:8080
-# 1.179.148.9:36476
-# 103.115.255.94:36331
